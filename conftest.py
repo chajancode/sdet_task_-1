0bc7062116184fb6ff0be45d4e33f76171d3d4ed
chajancode <89484766+chajancode@users.noreply.github.com>
2025-11-01 17:19:08 +0400

    Добавлен воркфлоу для guthub actions

--- conftest.py
+++ conftest.py
@@ -15,7 +15,9 @@ from pages.delete_customer import DeleteCustomer
 @pytest.fixture(scope="session")
 def browser() -> Generator[WebDriver, None, None]:
     chrome_options = Options()
-    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     driver: WebDriver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
         options=chrome_options
